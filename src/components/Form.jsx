f8599036365dbad0a5d10719e548fd76d044905f
Néstor Iriondo <nestoririondo@gmail.com>
2023-12-15 15:44:40 +0100

    local storage handled now with useEffect

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,38 +1,34 @@
 import React, { useState } from "react";
 
-const Form = ({ setTodos, handleLocalStorage }) => {
+const Form = ({ setTodos }) => {
 
   const [taskText, setTaskText] = useState(""); // This state should contain the taskText of the to-do
 
   const handleChange = (event) => setTaskText(event.target.value); // Updates the taskText state with each keystroke
 
-  const handleAdd = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (taskText.trim() === "") {
-      alert("Please write something");
-    } else {
-      const newId = Math.floor(Math.random() * 1000000000);
+    if (taskText.trim() === "") return;
 
-      const newTodo = {
-        id: newId,
-        title: taskText, // We are using the title state
-        done: false,
-        important: false,
-      }
+    const newId = Math.floor(Math.random() * 1000000000);
 
-      const updateTodos = (whateverWasThereBefore) => [...whateverWasThereBefore, newTodo];
-      // We are DECLARING a function that puts the new to-do at the end of the array
-      
-      setTodos(updateTodos); // And now we pass the function as an argument!!! damn
-      handleLocalStorage(newTodo)
-      setTaskText("");
+    const newTodo = {
+      id: newId,
+      title: taskText, // We are using the title state
+      done: false,
+      important: false,
     }
+
+    const updateTodos = (whateverWasThereBefore) => [...whateverWasThereBefore, newTodo]; // We are DECLARING a function that puts the new to-do at the end of the array   
+    setTodos(updateTodos); // And now we pass the function as an argument!!! damn
+
+    setTaskText("");
     }
 
   return (
     <div className="block">
-      <form onSubmit={handleAdd}>
+      <form onSubmit={handleSubmit}>
             <input
               onChange={handleChange}
               type="text"
